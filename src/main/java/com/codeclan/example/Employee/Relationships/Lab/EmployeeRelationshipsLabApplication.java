01c5efb8d22affe7664625d7930ea24fe6db76db
Ronan Hughes <rhughes490@gmail.com>
2020-08-04 10:14:10 +0100

    First commit

---
+++ src/main/java/com/codeclan/example/Employee/Relationships/Lab/EmployeeRelationshipsLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.Employee.Relationships.Lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRelationshipsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRelationshipsLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/Employee/Relationships/Lab/EmployeeRelationshipsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.Employee.Relationships.Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRelationshipsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
